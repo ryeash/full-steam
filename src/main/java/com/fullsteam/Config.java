70e32f0f2310ba5ee375f50b571efbd2ca9b2e52
ryeash <ryan.yeash@broadcom.com>
2025-08-01 20:43:03 -0600

    wip

--- src/main/java/com/fullsteam/Config.java
+++ src/main/java/com/fullsteam/Config.java
@@ -8,10 +8,6 @@ import java.util.function.Function;
 public class Config {
 
     // Helper methods to read from System Properties with defaults
-    private static boolean getBool(String name, boolean defaultValue) {
-        return getProp(name, defaultValue, Boolean::parseBoolean);
-    }
-
     private static int getInt(String name, int defaultValue) {
         return getProp(name, defaultValue, Integer::parseInt);
     }
@@ -68,7 +64,7 @@ public class Config {
     public static final double SPAWN_VERTICAL_PADDING = getDouble("game.spawn_padding_v", 50.0);
     public static final double SPAWN_MIDFIELD_BUFFER = getDouble("game.spawn_midfield_buffer", 100.0);
     public static final int MAX_PLAYERS_PER_TEAM = getInt("game.max_players_per_team", 5);
-    public static final int HAZARD_COUNT = getInt("game.hazard.count", 4);
+    public static final int HAZARD_COUNT = getInt("game.hazard.count", 2);
     public static final double HAZARD_SLOW_FACTOR = getDouble("game.hazard.slow_factor", 0.5); // 50% speed
     public static final double HAZARD_DAMAGE_FACTOR = getDouble("game.hazard.damage_factor", 0.5); // damage per tick;
 
