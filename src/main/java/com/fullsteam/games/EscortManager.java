44b3f0da9ce8fb98d0c7e72a7e34c41ff8c36c4b
ryeash <ryan.yeash@broadcom.com>
2025-08-01 20:58:45 -0600

    wip

--- src/main/java/com/fullsteam/games/EscortManager.java
+++ src/main/java/com/fullsteam/games/EscortManager.java
@@ -117,7 +117,6 @@ public class EscortManager extends AbstractTeamBasedManager {
             sendGameEvent(GameEvent.red("The payload was not delivered in time"));
             return true;
         }
-        obstacles.clear();
         return false;
     }
 
