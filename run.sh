ab9822f2adb8104b455e3f6e7aca4cef80ce0ad0
ryeash <8922579+ryeash@users.noreply.github.com>
2025-08-04 18:16:52 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8080:8080 --rm -it $(docker build --build-arg BRANCH=wip1 --no-cache -q .)
+docker run -p 8080:8080 --rm -it $(docker build --build-arg BRANCH=master --no-cache -q .)
